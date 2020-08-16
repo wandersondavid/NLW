57551bd24953d333961736858f730ae56b4c00cf
Wanderson David <wandersondavid.ifg@gmail.com>
2020-08-16 12:09:59 -0300

    remove electron

--- electron/index.js
+++
@@ -1,8 +0,0 @@
-const { app, BrowserWindow} = require('electron')
-
-function creationWindow() {
-     const  win = new BrowserWindow({ width: 1099, height: 900 })
-    win.loadURL('http://localhost:4000');
-}
-
-app.on('ready', creationWindow)
--- public/electron/src/core/api/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-
-const api = axios.create({
-    baseURL: 'http://localhost:9999/electron'
-})
-
-export default api
