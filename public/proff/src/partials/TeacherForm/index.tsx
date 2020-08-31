2784457af079e8a2cbac158069cc84643b6d05f3
Wanderson David <wandersondavid.ifg@gmail.com>
2020-08-31 16:26:07 -0300

    Front feat

--- public/proff/src/partials/TeacherForm/index.tsx
+++ public/proff/src/partials/TeacherForm/index.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
 
+import Input from '../../components/Input';
+import './styles.css'
+
+
 function TeacherForm() {
     return (
         <div id="page-teacher-form" className="container">
-            <PageHeader title="teste"/>
+            <PageHeader title="Que incrível que você quer da aulas."
+                        description="O primeiro passo é preencher esse formulário de incrição"/>
+
+            <main>
+                <fieldset>
+                    <legend>Seus dados</legend>
+                    <Input name="name" label="Nome Completo" />
+                    <Input name="avatar" label="Avatar" />
+                    <Input name="whatsapp" label="Whatsapp" />
+                </fieldset>
+            </main>
         </div>
     )
 }
