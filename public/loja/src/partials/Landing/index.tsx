66c1749937b8236f74db8d3a7ef9faf47a3f166c
Wanderson David <wandersondavid.ifg@gmail.com>
2020-09-30 00:04:20 -0300

    refactor code

--- public/loja/src/partials/Landing/index.tsx
+++ public/loja/src/partials/Landing/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logoImg from '../../assets/images/logo.svg';
+import tenisImg from '../../assets/images/adidas.png';
 import landingImg from '../../assets/images/landing.svg';
 import  { Link } from 'react-router-dom'
 
@@ -14,26 +14,14 @@ function Landing() {
      return(
          <div id="page-landing">
              <div id="page-landing-content" className="content">
-                 <div className="logo-conteiner">
-                     <img src={logoImg} alt="Proffy"/>
-                     <h2>Sua plataforma de estudo online.</h2>
+                 <div className="title-conteiner">
+                    <h2>Sua plataforma de estudo online.</h2>
+                    <h3>Sua plataforma de estudo online.</h3>
                  </div>
-                 <img src={landingImg} alt="Plataforma de estudo" className="hero-image"/>
 
+                 <img src={tenisImg} alt="Plataforma de estudo" className="hero-image"/>
                  <div className="button-container">
-                     <Link to="/study" className="study">
-                        <img src={studyIcon} alt="Estudar"/>
-                        Estudar
-                     </Link>
-                     <Link to="/give-classes" className="give-classes">
-                        <img src={giveClassIcon} alt="Dar aulas"/>
-                        Dar aulas
-                     </Link>
                 </div>
-                <span className="total-connections">
-                    Total de 200 conexão já realizadas
-                    <img src={purpleHeartIcon} alt="Coração roxo"/>
-                </span>
             </div>
          </div>
      )
